7c3001b962aa0ed73a7b7cffb2292d0abeaaeeeb
kijookijoo <kjyoon0125@gmail.com>
2026-02-25 21:03:29 -0800

    style changes

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -31,14 +31,13 @@ export function AppHeader() {
 
                 <div className='items-left'>
                     <Link className='navbar-item brand-link' to='/' style={{ margin: "0px" }}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon"><path d="M12 16v5" /><path d="M16 14v7" /><path d="M20 10v11" /><path d="m22 3-8.646 8.646a.5.5 0 0 1-.708 0L9.354 8.354a.5.5 0 0 0-.707 0L2 15" /><path d="M4 18v3" /><path d="M8 14v7" /></svg>
-
+                        <span className="brand-wordmark">WISELY.</span>
                     </Link>
 
                     <form className="header-search" onSubmit={handleSearch}>
                         <input className="header-search-input"
                             type="text"
-                            placeholder="Enter ticker here..."
+                            placeholder="Search ticker here..."
                             aria-label="text"
                             required
                             minLength={1}
